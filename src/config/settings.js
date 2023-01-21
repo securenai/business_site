aa812c0bcc5bbb4950cabc6d64601ecc4324327c
Michael Cheng <michaelnccu@gmail.com>
2023-01-21 17:45:02 +0800

    name change test

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `ŞİRKET Sitesi`,
+	title: `恆自然開發有限公司`,
 	description: `Temiz bir web şablonu`,
 	url: `https://cemkaan.com`,
-	name: `Şirket`,
+	name: `恆自然開發有限公司`,
 	adres: `Voyvoda Caddesi (Bankalar Caddesi) No: 35-37 Karaköy Beyoğlu İstanbul`,
-	analytics: "" // analytics code
+	analytics: "", // analytics code
 };
